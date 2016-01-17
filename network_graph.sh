fc8ceb5330b5bd97fabe027fb4030f684b2045b9
root <root@asterix.lan>
2016-01-17 23:08:58 +0100

    prechecks functions added

--- network_graph.sh
+++ network_graph.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+#===KimkIL=== 2016(c)
 
 IFCONFIG="/sbin/ifconfig"
-INTERFACE="eth0"
+INTERFACE="et"
 WORKDIR="/root/rrd/TEST"
+WEBDIR="/var/www/html/monitoring"
+ACTUALDIR=$(pwd)
 RRDDB="$WORKDIR/$INTERFACE'.rrd'"
 HOST_IP=""
-#in=$(ssh $HOST_IP $IFCONFIG $INTERFACE | grep bytes | awk '{print $2}' | cut -d : -f 2)
-#out=$(ssh $HOST_IP $IFCONFIG $INTERFACE | grep bytes | awk '{print $6}' | cut -d : -f 2)
-in=$($IFCONFIG $INTERFACE | grep bytes| awk '{print $2}' | cut -d : -f 2)
-out=$($IFCONFIG $INTERFACE | grep bytes | awk '{print $6}' |  cut -d : -f 2)
+in=`$IFCONFIG $INTERFACE | grep bytes| awk '{print $2}' | cut -d : -f 2`
+out=`$IFCONFIG $INTERFACE | grep bytes | awk '{print $6}' |  cut -d : -f 2`
+
 #COLOR DEFINITIION
-#RED='\033[0;31m'
 GREEN='\033[0;32m'
 COLOR_END='\033['
 SWITCH="\033["
@@ -18,83 +19,153 @@ NORMAL='${SWITCH}0m'
 YELLOW="${SWITCH}1;33m"
 RED="${SWITCH}0;31m"
 RESET="\x1B[0m"
-echo $RRDDB
 
-#check_interface=`$IFCONFIG $INTERFACE`
-echo "checking interface: $INTERFACE"
-if $IFCONFIG $INTERFACE &>> /dev/null; then
-	echo "$INTERFACE exist"
-	echo  -e "CHECK INTERFACE :" "$GREEN OK $RESET"
-	echo "Checking Round Robin Database"
 
-#reate RRD if doesnt exist
-if [ -e $RRDDB ];then
-        echo "Round Robin Database $RRDDB already exist"
+#Prechecks functions
+
+#checking if is RRDTOOL installed on system
+#return codes: 1 error ; 0 ok ; 3 UNKNOWN
+function check_rrdtool {
+	os=$(lsb_release -si)
+	if [ $os == "Debian" ]; then
+		if  dpkg -s rrdtool &>/dev/null; then
+			return 0
+		else
+			apt-get update && apt-get -y install rrdtool
+			if dpkg -s rrdtool &> /dev/null;then
+				return 0
+			else
+				return 1
+			fi
+		fi
+	fi
+return 3
+}
+check_rrdtool
+result_check_rrdtool=$?
+
+
+function check_interface {
+	if $IFCONFIG $INTERFACE &>/dev/null ; then
+		if [ -z $out ] && [ -z $in ];then #check if variable out and in are empty
+			return 1
+		else
+			return 0	
+		fi
+	else
+		return 1
+
+	fi
+return 3
+}
+check_interface
+result_check_interface=$?
+
+#checking if paths exist
+#WORKDIR and WEBDIR
+function check_paths {
+	if [ -d $WORKDIR ] && [ -d $WEBDIR ];then
+		return 0
+	else
+		if [ ! -d $WORKDIR ];then
+			mkdir -p $WORKDIR
+		fi
+		if [ ! -d $WEBDIR ];then
+			mkdir -p $WEBDIR
+		fi
+		
+		if [ -d $WORKDIR ] && [ -d $WEBDIR ];then
+			return 0
+		else
+			return 1
+		fi
+		
+	fi
+}
+check_paths
+result_check_paths=$?
+
+
+
+#Create RRD if doesnt exist
+if [ $result_check_rrdtool == "0" ] && [ $result_check_interface == "0" ] && [ $result_check_paths == "0" ]; then
+  
+
+
+	if [ -e $RRDDB ];then
+        	echo "Round Robin Database $RRDDB already exist"
+	else
+
+        	echo "Round Robin Database $RRDDB doesnt exist"
+        	echo "Creating DB..."
+        	rrdtool create $RRDDB \
+        	--step 60 \
+        	--step 60 \
+        	DS:$INTERFACE'_rx':COUNTER:120:0:U \
+        	DS:$INTERFACE'_tx':COUNTER:120:0:U \
+        	RRA:AVERAGE:0.5:1:3600 \
+        	RRA:MAX:0.5:1:3600
+        	if [ -a $RRDDB ];then
+                	echo "RRD database $RRDDB was created"
+                	echo "OK"
+        	fi
+	fi
+
+	rrdupdate $RRDDB N:$in:$out
+
+	rrdtool graph $WORKDIR/$INTERFACE'_hourly.png' --start end-3600s   \
+        	-a PNG -t "Hourly - Network $HOST_IP Interface $INTERFACE" --vertical-label "bits/s" \
+        	-w 1260 -h 800 -r \
+        	DEF:$INTERFACE'_rx'=$RRDDB:$INTERFACE'_rx':AVERAGE \
+        	DEF:$INTERFACE'_tx'=$RRDDB:$INTERFACE'_tx':AVERAGE \
+        	CDEF:$INTERFACE'_rxb'=$INTERFACE'_rx',-8,\* \
+        	CDEF:$INTERFACE'_txb'=$INTERFACE'_tx',8,\* \
+        	AREA:$INTERFACE'_rxb'#EC9D48:_$INTERFACE'-RX' \
+        	AREA:$INTERFACE'_txb'#ECD748:_$INTERFACE'-TX' \
+        	LINE1:$INTERFACE'_rxb'#CC7016:_$INTERFACE'-RX' \
+        	LINE1:$INTERFACE'_txb'#C9B215:_$INTERFACE'-TX'
+
+	#daily
+	rrdtool graph $WORKDIR/$INTERFACE'_daily.png' --start end-86400s   \
+        	-a PNG -t "Daily - Network OpenWRT Interface $INTERFACE" --vertical-label "bits/s" \
+        	-w 1260 -h 800 -r \
+        	DEF:$INTERFACE'_rx'=$RRDDB:$INTERFACE'_rx':AVERAGE \
+        	DEF:$INTERFACE'_tx'=$RRDDB:$INTERFACE'_tx':AVERAGE \
+        	CDEF:$INTERFACE'_rxb'=$INTERFACE'_rx',-8,\* \
+        	CDEF:$INTERFACE'_txb'=$INTERFACE'_tx',8,\* \
+        	AREA:$INTERFACE'_rxb'#EC9D48:$INTERFACE'-RX' \
+        	AREA:$INTERFACE'_txb'#ECD748:$INTERFACE'-TX' \
+        	LINE1:$INTERFACE'_rxb'#CC7016:$INTERFACE'-RX' \
+        	LINE1:$INTERFACE'_txb'#C9B215:$INTERFACE'-TX'
+
+	#create weekly graph
+	rrdtool graph $WORKDIR/$INTERFACE'_weekly.png' --start end-604800s   \
+        	-a PNG -t "Weekly - Network OpenWRT Interface $INTERFACE" --vertical-label "bits/s" \
+        	-w 1260 -h 800 -r \
+        	DEF:$INTERFACE'_rx'=$RRDDB:$INTERFACE'_rx':AVERAGE \
+        	DEF:$INTERFACE'_tx'=$RRDDB:$INTERFACE'_tx':AVERAGE \
+        	CDEF:$INTERFACE'_rxb'=$INTERFACE'_rx',-8,\* \
+        	CDEF:$INTERFACE'_txb'=$INTERFACE'_tx',8,\* \
+        	AREA:$INTERFACE'_rxb'#D7CC00:$INTERFACE'-RX' \
+        	AREA:$INTERFACE'_txb'#D7CC00:$INTERFACE'-TX' \
+        	LINE2:$INTERFACE'_rxb'#0101D6:_$INTERFACE'-RX' \
+        	LINE2:$INTERFACE'_txb'#00D730:_$INTERFACE'-TX'
+
+
+
+	cp $WORKDIR/$INTERFACE'_hourly.png' $WEBDIR/$INTERFACE'_hourly.png'
+	cp $WORKDIR/$INTERFACE'_daily.png' $WEBDIR/$INTERFACE'_daily.png'
+	cp $WORKDIR/$INTERFACE'_weekly.png' $WEBDIR/$INTERFACE'_weekly.png'
+
 else
-
-        echo "Round Robin Database $RRDDB doesnt exist"
-        echo "Creating DB..."
-        rrdtool create $RRDDB \
-        --step 60 \
-        --step 60 \
-        DS:$INTERFACE'_rx':COUNTER:120:0:U \
-        DS:$INTERFACE'_tx':COUNTER:120:0:U \
-        RRA:AVERAGE:0.5:1:3600 \
-        RRA:MAX:0.5:1:3600
-        if [ -a $RRDDB ];then
-                echo "RRD database $RRDDB was created"
-                echo "OK"
-        fi
-fi
-echo $in $out
-rrdupdate $RRDDB N:$in:$out
-
-rrdtool graph $WORKDIR/$INTERFACE'_hourly.png' --start end-3600s   \
-        -a PNG -t "Hourly - Network $HOST_IP Interface $INTERFACE" --vertical-label "bits/s" \
-        -w 1260 -h 800 -r \
-        DEF:$INTERFACE'_rx'=$RRDDB:$INTERFACE'_rx':AVERAGE \
-        DEF:$INTERFACE'_tx'=$RRDDB:$INTERFACE'_tx':AVERAGE \
-        CDEF:$INTERFACE'_rxb'=$INTERFACE'_rx',-8,\* \
-        CDEF:$INTERFACE'_txb'=$INTERFACE'_tx',8,\* \
-        AREA:$INTERFACE'_rxb'#EC9D48:_$INTERFACE'-RX' \
-        AREA:$INTERFACE'_txb'#ECD748:_$INTERFACE'-TX' \
-        LINE1:$INTERFACE'_rxb'#CC7016:_$INTERFACE'-RX' \
-        LINE1:$INTERFACE'_txb'#C9B215:_$INTERFACE'-TX'
-
-#daily
-rrdtool graph $WORKDIR/$INTERFACE'_daily.png' --start end-86400s   \
-        -a PNG -t "Daily - Network OpenWRT Interface $INTERFACE" --vertical-label "bits/s" \
-        -w 1260 -h 800 -r \
-        DEF:$INTERFACE'_rx'=$RRDDB:$INTERFACE'_rx':AVERAGE \
-        DEF:$INTERFACE'_tx'=$RRDDB:$INTERFACE'_tx':AVERAGE \
-        CDEF:$INTERFACE'_rxb'=$INTERFACE'_rx',-8,\* \
-        CDEF:$INTERFACE'_txb'=$INTERFACE'_tx',8,\* \
-        AREA:$INTERFACE'_rxb'#EC9D48:$INTERFACE'-RX' \
-        AREA:$INTERFACE'_txb'#ECD748:$INTERFACE'-TX' \
-        LINE1:$INTERFACE'_rxb'#CC7016:$INTERFACE'-RX' \
-        LINE1:$INTERFACE'_txb'#C9B215:$INTERFACE'-TX'
-
-#create weekly graph
-rrdtool graph $WORKDIR/$INTERFACE'_weekly.png' --start end-604800s   \
-        -a PNG -t "Weekly - Network OpenWRT Interface $INTERFACE" --vertical-label "bits/s" \
-        -w 1260 -h 800 -r \
-        DEF:$INTERFACE'_rx'=$RRDDB:$INTERFACE'_rx':AVERAGE \
-        DEF:$INTERFACE'_tx'=$RRDDB:$INTERFACE'_tx':AVERAGE \
-        CDEF:$INTERFACE'_rxb'=$INTERFACE'_rx',-8,\* \
-        CDEF:$INTERFACE'_txb'=$INTERFACE'_tx',8,\* \
-        AREA:$INTERFACE'_rxb'#D7CC00:$INTERFACE'-RX' \
-        AREA:$INTERFACE'_txb'#D7CC00:$INTERFACE'-TX' \
-        LINE2:$INTERFACE'_rxb'#0101D6:_$INTERFACE'-RX' \
-        LINE2:$INTERFACE'_txb'#00D730:_$INTERFACE'-TX'
-
-
-
-cp $WORKDIR/$INTERFACE'_hourly.png' /var/www/html/TEST/$INTERFACE'_hourly.png'
-cp $WORKDIR/$INTERFACE'_daily.png' /var/www/html/TEST/$INTERFACE'_daily.png'
-cp $WORKDIR/$INTERFACE'_weekly.png' /var/www/html/TEST/$INTERFACE'_weekly.png'
-
-else
-
-	echo -e "Interface $INTERFACE doesnt exist:$RED failed $RESET "
+	if [ $result_check_interface == "1" ];then
+		echo -e "Interface $RED $INTERFACE $RESET doesnt exist : $RED FAILED $RESET "
+	fi
+	if [ $result_check_paths == "1" ];then
+		echo -e "Paths $WEBDIR and $WORKDIR doesnt exist and cannot be created: $RED FAILED $RESET"
+	fi
+	if [ $result_check_rrdtool == "1" ];then
+		echo -e "RRDTOOL is not installed and try to install $RED FAILED $RESET"
+	fi
 fi
 	
