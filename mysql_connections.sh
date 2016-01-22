64a4f656c365dd7945cdd5d1701075b8f2b77ece
kimkil94 <root@asterix.lan>
2016-01-22 19:22:34 +0100

    added mysql_connections stats

---
+++ mysql_connections.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#mysql_connections.sh - Connections statistics
+#
+
+WORKDIR="/var/lib/rrd"
+DB=$WORKDIR/mysql_connections.rrd
+WEBDIR=/var/www/html/stats
+RUNDIR="/opt/rrd/rrdtool"
+CRONTAB="/var/spool/cron/crontabs/root"
+
+established_conn=$(netstat -an | grep :443 | grep ESTABLISHED | wc -l)
+timewait_conn=$(netstat -an | grep :443 | grep TIME_WAIT| wc -l)
+echo $established_conn
+echo $timewait_conn
+
+if [ ! -e $DB ]
+then 
+	rrdtool create $DB \
+	--step 60 \
+	DS:established:GAUGE:120:0:5000  \
+	DS:timewait:GAUGE:120:0:50000 \
+	RRA:MAX:0.5:1:288 \
+	RRA:AVERAGE:0.5:1:288 
+fi
+
+
+#update RRDB
+rrdtool update $DB N:$established_conn:$timewait_conn
+
+#creating graphs in periods: hour, day, week,month,year
+for period in hour day week month year
+do
+
+	rrdtool graph $WEBDIR/mysq_connections-$period.png -w 785 -h 120 -a PNG --slope-mode -s -1$period --end now \
+	--vertical-label "HTTP Connections" \
+	DEF:established=$DB:established:AVERAGE \
+	DEF:timewait=$DB:timewait:AVERAGE \
+	AREA:established#f007:established_conn \
+	AREA:timewait#f005:timewait_conn 
+	 
+done
+
+#create cron task 
+if grep $RUNDIR/mysql_connections.sh /var/spool/cron/crontabs/root;then
+	echo "cron task already set"
+else
+echo "creating cron task..."
+echo "  *  *  *  *  *  $RUNDIR/connections_graph.sh" >> $CRONTAB
+fi
