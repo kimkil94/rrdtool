cadb1a4f355609a53b3cf0b547927d8f8f14b82d
root <root@dev.matsuko>
2016-10-10 16:35:16 +0200

    colors

--- build.html.sh
+++ build.html.sh
@@ -2,10 +2,15 @@
 
 for graph in `ls /var/www/html/stats | grep "hour"`;do
 	service=$(echo $graph | awk -F "-" '{print $1}')
+	echo "<body bgcolor="#708090"> </body>" >> /var/www/html/stats/index.html
+	echo "<div align="center">" >> /var/www/html/stats/index.html
 	echo "<a href="$service.html">" >> /var/www/html/stats/index.html
 	echo "<img src="$graph">" >> /var/www/html/stats/index.html
 	echo "</a>" >> /var/www/html/stats/index.html
+	echo "<br>" >> /var/www/html/stats/index.html
  	for graphs in `ls /var/www/html/stats/ | grep "$service"`;do
 		echo "<img src="$graphs">" >> /var/www/html/stats/$service.html
+		#echo "<br>" >> /var/www/html/stats/index.html
 	done
+	echo "</div>" >> /var/www/html/stats/index.html
 done
